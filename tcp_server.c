7c1c434991f431ab23bcfcc84ae3aee8ce285d1e
xanderwhitby <xwhitby20@gmail.com>
2025-04-29 15:15:29 -0600

    made log function for debugging

---
+++ tcp_server.c
@@ -0,0 +1,19 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+
+// for logging
+
+void logging(){
+    FILE *fptr;
+
+    fptr = fopen("logging.txt", "w");
+
+    fclose(fptr);
+}
+
